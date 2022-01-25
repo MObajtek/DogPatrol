5ec0bf83d229a34bf0e801386904d9456c65e0d6
AnnaSierka <anna.sierka2313@gmail.com>
2022-01-25 16:46:18 +0100

    Gui

---
+++ src/okna/OwnerGUI.java
@@ -0,0 +1,21 @@
+package okna;
+
+import javax.swing.*;
+
+public class OwnerGUI {
+    private JPanel panel1;
+    private JTextField walkerAddressField;
+    private JTextField walkerNameField;
+    private JLabel walkerName;
+    private JLabel walkerAddress;
+    private JButton placeErrandButton;
+    private JComboBox currentErrandsComboBox;
+    private JLabel errandsList;
+    private JLabel Wallet;
+    private JTextField walletValue;
+    private JPanel mapa;
+    private JLabel Map;
+    private JButton transferMoneyButton;
+    private JRadioButton archivalErrandsRadioButton;
+    private JRadioButton currentErrandsRadioButton;
+}
